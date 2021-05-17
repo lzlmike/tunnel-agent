ba03e4ca23fc9c6072ad2cbb4e03cc723574ddb4
Zuolin Li  (Mike) <lzlli@ucdavis.edu>
2021-05-17 06:37:28 -0700

    fix host servername

--- index.js
+++ index.js
@@ -201,8 +201,13 @@ function createSecureSocket(options, cb) {
   var self = this
   TunnelingAgent.prototype.createSocket.call(self, options, function(socket) {
     // 0 is dummy port for v0.6
+    let servername = null;
+    try {
+      servername = /host: (.*)\r/.exec(options.request._header)[1].trim();
+    } catch(e) {}
+
     var secureSocket = tls.connect(0, mergeOptions({}, self.options,
-      { servername: options.host
+      { servername: servername || options.host
       , socket: socket
       }
     ))
